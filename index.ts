9bd135834f94821b36ae6c126e05413b46b5828f
judeProground <jude@proground.co.kr>
2024-02-10 14:20:01 +0900

    4.2.3 The only case where a switch is allowed 4.2.4 Rule: Do not use switches

--- index.ts
+++ index.ts
@@ -19,7 +19,7 @@ enum RawTile {
 
 let playerx = 1;
 let playery = 1;
-let map: RawTile[][] = [
+let rawMap: RawTile[][] = [
   [2, 2, 2, 2, 2, 2, 2, 2],
   [2, 3, 0, 1, 1, 2, 0, 2],
   [2, 4, 2, 6, 1, 2, 0, 2],
@@ -27,6 +27,52 @@ let map: RawTile[][] = [
   [2, 4, 1, 1, 1, 9, 0, 2],
   [2, 2, 2, 2, 2, 2, 2, 2],
 ];
+
+let map: Tile[][];
+function assetExhausted(x: never): never {
+  throw new Error(`Unexpected object: ` + x);
+}
+
+function transformTile(raw: RawTile) {
+  switch (raw) {
+    case RawTile.AIR:
+      return new Air();
+    case RawTile.FLUX:
+      return new Flux();
+    case RawTile.UNBREAKABLE:
+      return new Unbreakable();
+    case RawTile.PLAYER:
+      return new Player();
+    case RawTile.STONE:
+      return new Stone();
+    case RawTile.FALLING_STONE:
+      return new FallingStone();
+    case RawTile.BOX:
+      return new Box();
+    case RawTile.FALLING_BOX:
+      return new FallingBox();
+    case RawTile.KEY1:
+      return new Key1();
+    case RawTile.LOCK1:
+      return new Lock1();
+    case RawTile.KEY2:
+      return new Key2();
+    case RawTile.LOCK2:
+      return new Lock2();
+    default:
+      return assetExhausted(raw);
+  }
+}
+
+function transformMap() {
+  map = new Array();
+  for (let y = 0; y < rawMap.length; y++) {
+    map[y] = new Array(rawMap[y].length);
+    for (let x = 0; x < rawMap[y].length; x++) {
+      map[y][x] = transformTile(rawMap[y][x]);
+    }
+  }
+}
 
 let inputs: Input[] = [];
 
@@ -172,6 +218,7 @@ function gameLoop() {
 }
 
 window.onload = () => {
+  transformMap();
   gameLoop();
 };
 
