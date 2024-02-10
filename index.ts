2b200183255b612ce17a1c6f42f3284ad326e51d
judeProground <jude@proground.co.kr>
2024-02-10 14:20:55 +0900

    Fix function call syntax

--- index.ts
+++ index.ts
@@ -178,12 +178,12 @@ function createGraphics() {
 }
 
 function colorOfTile(g: CanvasRenderingContext2D, x: number, y: number) {
-  if (map[y][x].isFlux) g.fillStyle = "#ccffcc";
-  else if (map[y][x].isUnbreakable) g.fillStyle = "#999999";
-  else if (map[y][x].isStone || map[y][x].isFallingStone) g.fillStyle = "#0000cc";
-  else if (map[y][x].isBox || map[y][x].isFallingBox) g.fillStyle = "#8b4513";
-  else if (map[y][x].isKey1 || map[y][x].isLock1) g.fillStyle = "#ffcc00";
-  else if (map[y][x].isKey2 || map[y][x].isLock2) g.fillStyle = "#00ccff";
+  if (map[y][x].isFlux()) g.fillStyle = "#ccffcc";
+  else if (map[y][x].isUnbreakable()) g.fillStyle = "#999999";
+  else if (map[y][x].isStone() || map[y][x].isFallingStone()) g.fillStyle = "#0000cc";
+  else if (map[y][x].isBox() || map[y][x].isFallingBox()) g.fillStyle = "#8b4513";
+  else if (map[y][x].isKey1() || map[y][x].isLock1()) g.fillStyle = "#ffcc00";
+  else if (map[y][x].isKey2() || map[y][x].isLock2()) g.fillStyle = "#00ccff";
 }
 
 function drawMap(g: CanvasRenderingContext2D) {
