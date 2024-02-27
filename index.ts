1c013d678dcea893eb1bb23ffa69a4baab92acac
j1h00 <guruboy@snu.ac.kr>
2024-02-27 22:32:01 +0900

    5.5 Integrating similar functions strategy pattern - 2

--- index.ts
+++ index.ts
@@ -460,6 +460,8 @@ function check(tile: Tile) {
   return tile.isLock1();
 }
 
+class RemoveStrategy {}
+
 function removeLock1() {
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
